fb32128808300d90e165eb2c37cec40a2eb8d613
LEO ARNAUD <leo.arnaud@iut.local>
2026-09-23 10:30:15 +0200

    tp du 23/09

---
+++ src/main/java/iut/info2/pays/Pays.java
@@ -0,0 +1,20 @@
+/*
+ * Pays                               23/09/2026
+ * IUT de Rodez, pas de copyright (ni de "copyleft")
+ */
+
+package iut.info2.pays;
+
+import java.util.TreeSet;
+
+/**
+ * TODO : écrire le commentaire javadoc
+ *
+ * @author léo arnaud
+ */
+public class Pays {
+
+    private String nomPays;
+
+    private;
+}
